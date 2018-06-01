bd28006249985119aae68d5ddd31825ca7282596
air.sky.one <air.sky.one@gmail.com>
2018-06-01 22:51:41 +0200

    Retrouve les jeux distincts d'une franchise Reste à sauvegarder

--- RomsFranchiseAssociation.py
+++ RomsFranchiseAssociation.py
@@ -9,11 +9,27 @@ from whoosh.fields import Schema, TEXT, KEYWORD, ID, STORED
 from whoosh.analysis import StemmingAnalyzer
 from whoosh.qparser import QueryParser
 
-# Set the Romlist file path
-romlistPath = input("Romlist file path (ex:/home/me/Documents/myFile.txt) : ")
+def GetDistinct(tab):
+    result = []
+    for e in tab:
+        if not e in result:
+            result.append(e)
+    return result
 
-if romlistPath == "x":
-    romlistPath = "/home/akerbage/Workspace/Projects/RetroGaming/urukai-romlist-database/--archives--/Python/1-RomlistHelper/Hyperpie/Nintendo Super Nintendo Entertainment System.txt"
+def FormatHitResults(r):
+    result = []
+    for e in r:
+        result.append(e["Name"])
+    return result
+'''
+GOOOOOOOOOOOOOOOOOOOOOOOOOOOO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+'''
+
+# Set the Romlist file path
+# romlistPath = input("Romlist file path (ex:/home/me/Documents/myFile.txt) : ")
+
+#if romlistPath == "x":
+romlistPath = "/home/urukai/Workspace/Retrogaming/Python scrap builders/urukai-romlist-database/--archives--/Python/1-RomlistHelper/Hyperpie/Nintendo Super Nintendo Entertainment System.txt"
 
 # convert the romlist into a list of name
 games = HelperRomlist.LoadRomListFile(romlistPath)
@@ -29,36 +45,41 @@ ix = index.create_in("indexdir", schema)
 ix = index.open_dir("indexdir")
 
 # Indexation des documents (g.gamename)
+print("Building index ...")
 writer = ix.writer()
 for g in games:
     writer.add_document(Name=g.gamename)
 writer.commit()
 
-# Load franchises list
+# Load Universes list
 tmpStr = HelperFile.GetFilecontent("data/FranchiseList.txt")
 lines = tmpStr.split("\n")
+
+'''with ix.searcher() as searcher:
+    t = "Super Mario"
+    print(t)
+    query = QueryParser("Name", ix.schema).parse(t)
+    results = searcher.search(query, groupedby="Name", limit=None)
+
+    if len(results) > 0 :
+        print("*Univers : " + t + " | Game : " + str(results[0]))
+
+        print(results.groups("Name"))
+        for r in results:
+            print(r["Name"])
+input("Please kill me ...")'''
 
 for l in lines:
     with ix.searcher() as searcher:
         print(l)
         query = QueryParser("Name", ix.schema).parse(l)
-        results = searcher.search(query)
+        results = searcher.search(query, limit=None)
 
         if len(results) > 0 :
-            print("Franchise : " + l + " | Game : " + str(results[0]))
-
-'''
-        # Résultats
-        found = results.scored_length()
-        if results.has_exact_length():
-            print("Scored", found, "of exactly", len(results), "documents")
-        else:
-            low = results.estimated_min_length()
-            high = results.estimated_length()
-
-            print("Scored", found, "of between", low, "and", high, "documents")
-
-            for r in results:
-                print(r)
-
-'''
+            formattedHits = FormatHitResults(results)
+            r = GetDistinct(formattedHits)
+            for e in r:
+                print(e)
+            input("Wait ...")
+            # à ce stade on récupère la liste de jeux distincts par franchise
+            # reste à trouver le format de sauvergarde et sauvegarder
